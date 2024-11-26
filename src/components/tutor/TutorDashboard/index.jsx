07fc8d686f006d93105a48bc59e56751ab0582fa
tsherrifdeen <tsherrifdeen@gmail.com>
2024-11-26 13:44:43 +0100

    fix: routing

--- src/components/tutor/TutorDashboard/index.jsx
+++ src/components/tutor/TutorDashboard/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MyCourseDetail from "../My courses/mycourses";
 import Account from "../Account/Account";
 import Withdrawals from "../TutorDashboard/withdraw/Withdraw";
@@ -6,26 +6,24 @@ import Sidebar from "./sidebar";
 
 const Index = () => {
   return (
-    <Router>
-      <div>
-        <section className="flex">
-          {/* Sidebar */}
-          <div className="w-full lg:w-1/5">
-            <Sidebar />
-          </div>
+    <div>
+      <section className="flex">
+        {/* Sidebar */}
+        <div className="w-full lg:w-1/5">
+          <Sidebar />
+        </div>
 
-          {/* Main Content */}
-          <div className="w-full px-4 lg:px-0 lg:w-4/5">
-            <Routes>
-              {/* Define routes for each component */}
-              <Route path="/my-courses" element={<MyCourseDetail />} />
-              <Route path="/account" element={<Account />} />
-              <Route path="/withdrawals" element={<Withdrawals />} />
-            </Routes>
-          </div>
-        </section>
-      </div>
-    </Router>
+        {/* Main Content */}
+        <div className="w-full px-4 lg:px-0 lg:w-4/5">
+          <Routes>
+            {/* Define routes for each component */}
+            <Route path="/my-courses" element={<MyCourseDetail />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/withdrawals" element={<Withdrawals />} />
+          </Routes>
+        </div>
+      </section>
+    </div>
   );
 };
 
