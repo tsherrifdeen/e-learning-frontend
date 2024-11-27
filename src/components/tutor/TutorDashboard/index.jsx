ea1f750bd0a9798e04b9eb383c8030ff8404b662
Your New Username <danieokandeji@gmail.com>
2024-11-27 15:46:30 +0100

    style

--- src/components/tutor/TutorDashboard/index.jsx
+++ src/components/tutor/TutorDashboard/index.jsx
@@ -1,32 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MyCourseDetail from "../My courses/mycourses";
-import Account from "../Account/Account";
-import Withdrawals from "../TutorDashboard/withdraw/Withdraw";
-import Sidebar from "./sidebar";
 
-const Index = () => {
+// import Dashboards from "./Dashboard"
+import MyCourseDetail from "../My courses/mycourses"
+// import Messages from "./Messages"
+import Account from "../Account/Account"
+import Withdrawals from "../TutorDashboard/withdraw/Withdraw"
+import Sidebar from "./sidebar"
+
+
+const index = () => {
   return (
-    <Router>
-      <div>
-        <section className="flex">
-          {/* Sidebar */}
-          <div className="w-full lg:w-1/5">
-            <Sidebar />
-          </div>
-
-          {/* Main Content */}
-          <div className="w-full px-4 lg:px-0 lg:w-4/5">
-            <Routes>
-              {/* Define routes for each component */}
-              <Route path="/my-courses" element={<MyCourseDetail />} />
-              <Route path="/account" element={<Account />} />
-              <Route path="/withdrawals" element={<Withdrawals />} />
-            </Routes>
-          </div>
-        </section>
+    <div>
+         <section className="flex    ">
+      {/* Sidebar */}
+      <div className="w-full lg:w-1/5">
+        <Sidebar />
       </div>
-    </Router>
-  );
-};
 
-export default Index;
+      {/* Main Content */}
+      <div className="w-full px-4 lg:px-0 lg:w-4/5">
+        {/* <Dashboards/> */}
+        <MyCourseDetail/>
+        {/* <Messages/> */}
+        <Account/>
+        <Withdrawals/>
+      </div>
+    </section>
+    </div>
+  )
+}
+
+export default index
