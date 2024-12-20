95c6f2e5959dd598d34e0a1af5c2cbad0dcf78d8
Your New Username <danieokandeji@gmail.com>
2024-12-20 14:52:02 +0100

    syle

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,11 +1,13 @@
 import HeraderImg from "../../assets/Background.png";
 import BlogCard from "../../components/Blog/Blog-card";
+import Blogs from "./Blog"
 
 const Blog = () => {
   return (
     <main className="mt-28 md:mt-6 w-[90%] mx-auto">
       <div>
         <img src={HeraderImg} alt="" />
+        <Blogs/>
         <BlogCard>
           <h3 className="text-xl font-bold">
             Popular<span className="text-[#F0A305] px-2">Articles</span>
