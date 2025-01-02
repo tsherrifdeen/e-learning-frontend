17f5a5614eced860fb1583af749823a385818b8f
Your New Username <danieokandeji@gmail.com>
2025-01-03 00:23:22 +0100

    style

--- src/components/UserProfile/Notifications/Notification.jsx
+++ src/components/UserProfile/Notifications/Notification.jsx
@@ -1,11 +1,31 @@
+import { Icon } from "@iconify/react";
 
-
-const Notification = () => {
+const NoNotifications = () => {
   return (
-    <div>
-      hi
-    </div>
-  )
-}
+    <div className="absolute top-1/4 left-0 right-0 flex items-center justify-center px-4">
+      <div className="bg-white rounded-2xl p-6 sm:p-8 max-w-md sm:max-w-xl w-full text-center border ">
+        {/* Icon */}
+        <div className="flex justify-center mb-4">
+          <Icon
+            icon="hugeicons:note-04"
+            className="text-blue"
+            width="80" // Default size for smaller screens
+            height="80"
+            style={{ maxWidth: "100px", maxHeight: "100px" }} // For larger screens
+          />
+        </div>
 
-export default Notification
+        {/* Message */}
+        <h1 className="text-lg sm:text-xl font-semibold text-gray-800">
+          No new notifications for now!
+        </h1>
+        <p className="text-sm sm:text-base text-gray-500 mt-2">
+          Don’t worry, we’ll keep you updated with exciting news and updates.
+          Stay tuned!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NoNotifications;
